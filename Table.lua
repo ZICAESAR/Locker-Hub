2f78ec21144b88156e875ad223275a66a4fa3b34
Lobby$ZISAR <95739957+ZICAESAR@users.noreply.github.com>
2023-01-12 16:26:17 +0700

    Create Table.lua

---
+++ Table.lua
@@ -0,0 +1,14 @@
+local API = {}
+    if (Key) == "<???>" then
+        function API:FastAttack(Boolen)
+            if Boolen then
+                print("FastAttack")
+            end
+        end
+        function API:WebHook(Boolen)
+            if Boolen then
+                print("soon")
+            end
+        end
+    end
+return API
